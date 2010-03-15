720440768db7bf3c6585080c1032031454411395
Jason Diamond <jason@diamond.name>
2010-03-15 09:07:03 -0700

    Explicitly reference MIT license. Added headers to tool files.

--- BehaveN/ValueComparer.cs
+++ BehaveN/ValueComparer.cs
@@ -2,6 +2,8 @@
 //
 // Copyright (c) 2009-2010 Jason Diamond
 //
+// This source code is released under the MIT License.
+//
 // Permission is hereby granted, free of charge, to any person
 // obtaining a copy of this software and associated documentation
 // files (the "Software"), to deal in the Software without
