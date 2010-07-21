dc46ab17b60846e505c99853637dd9f13dd7ec01
Jason Diamond <jason@diamond.name>
2010-07-21 14:58:02 -0700

    Added BehaveN.GUI project.

---
+++ BehaveN.GUI/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+
+namespace BehaveN.GUI
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var d = new OpenFileDialog();
+
+            if (d.ShowDialog().GetValueOrDefault())
+            {
+                string fileName = d.FileName;
+                MessageBox.Show("You selected " + fileName + "!");
+            }
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
